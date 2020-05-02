14d63d43bcb9115c6a6608208df48f176bc2b767
zijie <zijie@test1.com>
2020-05-02 23:23:31 +0800

    Centos7系统基础优化配置
    	1. 设置时区并时间同步
    	2. 禁用seLinux
    	3. 清空防火墙
    	4. 历史命令显示时间操作
    	5. 设置超时退登
    	6. 禁止root远程登录
    	7. 禁止定时任务发送邮件
    	8. 设置打开最大文件数
    	9. 减少swap使用
    	10.系统内核参数优化
    	11.安装系统分析性能工具
    
     请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交

---
+++ init/init.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#设置时区
+timedatectl set-timezone Asia/Shanghai
+#同步时间
+if  echo 'rpm -qa |egrep ^ntp*.|wc -l' -eq 0;then
+        yum install -y ntp
+        systemctl start ntpd
+fi
+if ! crontab -l|grep ntpdate &>/dev/null ;then
+        (echo "* 1 * * * ntpdate ntp1.aliyun.com >/dev/null 2>&1";crontab -l) |crontab
+fi
+
+
+#禁用selinux
+setenforce 0 
+sed  -i '/^SELINUX=/c SELINUX=disabled' /etc/selinux/config
+
+#清空防火墙,停用firwalld，安装iptables
+systemctl stop firewalld
+systemctl disable firewalld
+yum install -y iptables\*
+systemctl start iptables
+systemctl enable iptables
+iptables -F
+iptables-save > /etc/sysconfig/iptables
+
+#history显示时间和用户名
+#对当前窗口暂时有效
+sed -i '$a export HISTTIMEFORMAT="%F %T `whoami` "' /etc/bashrc
+source /etc/bashrc
+
+#禁止root远程登录
+# !请注意一定要有能sudo的用户再禁止root用户远程登录
+#sed -i 's/#PermitRootLogin yes/PermitRootLogin on/' /etc/ssh/sshd_config
+
+#禁止定时任务发送邮件
+sed -i 's/^MAILTO=root/MAILTO=""/' /etc/crontab
+
+#设置最大文件打开数
+if ! grep "*       soft    nofile  65535" /etc/security/limits.conf &>/dev/null; then
+cat >> /etc/security/limits.conf <<EOF
+*       soft    nofile  65535
+*       hard    nofile  65535
+EOF
+
+fi
+
+#系统内核优化
+cat >> /etc/sysctl.conf <<EOF
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_max_tw_buckets = 20480
+net.core.netdev_max_backlog = 262144
+net.ipv4.tcp_max_syn_backlog = 20480
+net.ipv4.tcp_fin_timeout = 20
+EOF
+
+#减少swap的使用
+echo "0" > /proc/sys/vm/swappiness
+
+#配置阿里yum源
+budir=/etc/yum.repos.d/backup`date "+%Y%m%d"`
+mkdir $budir
+mv /etc/yum.repos.d/Cen*repo $budir
+yum install -y wget
+wget -P /etc/yum.repos.d/  http://mirrors.aliyun.com/repo/Centos-7.repo
+yum clean all
+yum makecache
+
+
+#安装系统性能分析工具以及其他
+yum install gcc make autoconf vim sysstat net-tool iostat iftop iotp lrzsz lsof wget curl -y
+
+
