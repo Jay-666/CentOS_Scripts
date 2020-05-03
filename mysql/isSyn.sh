0bbc510c6d84a8a2d02ee20da078d22bec738fc5
zijie <zijie@test1.com>
2020-05-03 22:39:24 +0800

    检测mysql主从同步是否正常

---
+++ mysql/isSyn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#检测主从同步工作是否正常
+#在从服务器执行，把脚本写入时间任务，并发邮件警报
+HOST=localhost
+USER=root
+PASSWD=123
+IO_SQL_STATUS=$(mysql -h$HOST -u$USER -p$PASSWD -e 'show slave status\G' 2>dev/null |awk '/Slavw_.*_Running:/print{$1$2}')
+for i in $IO_SQL_STATUS;do
+  STATUS_NAME=${i%:*}
+  STATUS=${i#*:}
+  if [ "$STATUS" != "Yes" ];then
+    echo "Error: MySQL Master-Slave $STATUS_NAME status is $STATUS"
+  fi
+done
