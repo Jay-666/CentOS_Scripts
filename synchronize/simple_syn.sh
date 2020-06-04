df60f1ff331cf1427a31bf17727acb7f0ca8eb55
ningzijie <ningzijie@test5.com>
2020-06-05 02:54:16 +0800

    变量可修改

--- synchronize/simple_syn.sh
+++ synchronize/simple_syn.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #本地单向同步
-MON_DIR=/opt
+
+MON_DIR=/data
+BACKUP_SERVER=192.168.0.16
+
 inotifywait -mqr -e create,move,delete,attrib,modify $MON_DIR |\
 while read event;do
-  rsync -a --delete /opt 192.168.0.15:/backup/192.168.0.13
-  echo "`date +%F\ %T` 出现事件$event" >> /var/log/rsync.log 2>&1
+  rsync -a --delete $MON_DIR $BACKUP_SERVER:/backup/
+  #echo "`date +%F\ %T` 出现事件$event" >> /var/log/rsync.log 2>&1
 done
 
